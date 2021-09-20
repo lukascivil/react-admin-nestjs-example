29be7bc4e46ca54baa84cc52a14d0e677c98904b
lukascivil <lukas_civil@hotmail.com>
2021-09-19 23:47:53 -0300

    fix: infinit loop with RTK

--- src/resources/custom/tab-local-list.tsx
+++ src/resources/custom/tab-local-list.tsx
@@ -5,7 +5,7 @@ import { useGetUsersQuery } from 'store/api/users-api'
 import LocalList from 'core/components/local-list'
 
 export const TabLocalList: FC = () => {
-  const [filter, setFilter] = useState({})
+  const [filter, setFilter] = useState({ email: '' })
   const currentSort = { field: 'created_at', order: 'ASC' }
   const { isFetching, data, refetch } = useGetUsersQuery({
     filter,
@@ -19,10 +19,13 @@ export const TabLocalList: FC = () => {
       data={data?.data}
       onRefresh={refetch}
       onSubmit={values => {
-        setFilter(values)
+        // Bug around here, when setFilter changes state with a new filter.
+        // This filter must contain the same key, otherwise it will imply
+        // an error in the API causing the RTK hook to loop.
+        setFilter(values as any)
       }}
       sort={currentSort}
-      filters={[<TextInput source="cafe" validate={required()} />]}
+      filters={[<TextInput source="email" validate={required()} />]}
     >
       <Datagrid>
         <TextField source="id" label="Id" />
