e9bf8b09eecb137fa2d15f74e4c2d534e4d3ab8d
lukascivil <lukas_civil@hotmail.com>
2024-07-02 18:07:39 -0300

    chore: migrate to monorepo

---
+++ apps/react-admin/node_modules/.vite/deps/react.js
@@ -0,0 +1,6 @@
+import {
+  require_react
+} from "./chunk-CMM6OKGN.js";
+import "./chunk-OL46QLBJ.js";
+export default require_react();
+//# sourceMappingURL=react.js.map
---
+++ apps/react-admin/node_modules/.vite/deps/react_jsx-runtime.js
@@ -0,0 +1,7 @@
+import {
+  require_jsx_runtime
+} from "./chunk-7BUGFXDR.js";
+import "./chunk-CMM6OKGN.js";
+import "./chunk-OL46QLBJ.js";
+export default require_jsx_runtime();
+//# sourceMappingURL=react_jsx-runtime.js.map
