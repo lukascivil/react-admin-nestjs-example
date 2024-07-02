0c3067e7cd90b1c1be801ccdc7fda334d238b64c
lukascivil <lukas_civil@hotmail.com>
2024-07-02 17:01:38 -0300

    Update handsontable-input.tsx

--- src/core/components/handsontable-input.tsx
+++ src/core/components/handsontable-input.tsx
@@ -212,7 +212,7 @@ export const HandsontableInput = <
         />
       </Box>
       <FormHelperText>
-        {(isTouched || isSubmitted) && <InputHelperText error={error?.message} helperText={helperText} />}
+        <InputHelperText error={error?.message} helperText={helperText} />
       </FormHelperText>
     </FormControl>
   )
