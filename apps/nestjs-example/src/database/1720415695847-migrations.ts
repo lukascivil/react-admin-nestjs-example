ef4a66678c0b29322579ff9cb20262b70e82f733
lukascivil <lukas_civil@hotmail.com>
2024-07-08 02:15:33 -0300

    chore: adjust migrations

---
+++ apps/nestjs-example/src/database/1720415695847-migrations.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migrations1720415695847 implements MigrationInterface {
+    name = 'Migrations1720415695847'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "email"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "email" character varying(256) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email")`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "email"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "email" character varying(257) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email")`);
+    }
+
+}
