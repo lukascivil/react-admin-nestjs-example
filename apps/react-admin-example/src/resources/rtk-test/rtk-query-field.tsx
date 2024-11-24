3393314f19e57ab53692f5f11e164d5bcca63c7a
lukascivil <lukas_civil@hotmail.com>
2024-11-24 00:35:05 -0300

    Update rtk-query-field.tsx

--- apps/react-admin-example/src/resources/rtk-test/rtk-query-field.tsx
+++ apps/react-admin-example/src/resources/rtk-test/rtk-query-field.tsx
@@ -5,13 +5,17 @@ import { RecordContextProvider, LinearProgress, useRecordContext } from 'react-a
 import { Error as ErrorIcon } from '@mui/icons-material'
 import { usersApi } from 'store/api/users-api'
 
-interface RtkQueryFieldProps<RecordType extends Record<string, any> = any> {
+type Endpoints = keyof typeof usersApi.endpoints
+
+type QueryArg<C extends Endpoints> = (typeof usersApi.endpoints)[C]['Types']['QueryArg']
+
+type RtkQueryFieldProps<RecordType extends Record<string, any> = any, C extends Endpoints = Endpoints> = {
   label: string
   record?: RecordType
   children: ReactElement
-  endpoint: string
+  endpoint: C
   emptyText?: string
-  args: (record?: RecordType) => any
+  args: (record?: RecordType) => QueryArg<C>
   queryOptions?: SubscriptionOptions & {
     skip?: boolean
     refetchOnMountOrArgChange?: boolean | number
@@ -25,17 +29,29 @@ interface RtkQueryFieldProps<RecordType extends Record<string, any> = any> {
  * @param props
  * @returns
  */
-const RtkQueryField = <RecordType extends Record<string, any> = any>(props: RtkQueryFieldProps<RecordType>) => {
+const RtkQueryField = <
+  RecordType extends Record<string, any> = any,
+  C extends keyof typeof usersApi.endpoints = keyof typeof usersApi.endpoints
+>(
+  props: RtkQueryFieldProps
+) => {
   const { children, emptyText, endpoint, args, queryOptions } = props
   const record = useRecordContext(props)
   const {
     data: referenceRecord,
     isError,
     isLoading
-  }: TypedUseQueryHookResult<RecordType, any, any> = usersApi.endpoints[endpoint].useQuery(args(record), {
-    skip: !args,
-    ...queryOptions
-  })
+  }: TypedUseQueryHookResult<RecordType, any, any> = usersApi.endpoints[endpoint as C].useQuery(
+    args(record) as QueryArg<C>,
+    {
+      skip: !args,
+      ...queryOptions
+    }
+  )
+
+  // const cafe: typeof usersApi.endpoints.getUsers.Types.QueryArg
+  // const cafe: Pick<typeof usersApi.endpoints, 'getUsers'>['getUsers']['Types']['QueryArg']
+  // const cafe: QueryArg<'getUsers'>
 
   console.log({ referenceRecord })
 
